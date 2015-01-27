20072d2276b96757f4a911654f98ab5cacb3df95
hermagrini <inirgammza05@gmail.com>
2015-01-26 23:10:18 -0300

    contact manager draft

---
+++ ContactManager/js/models/contact.js
@@ -0,0 +1,26 @@
+var app = app || {};
+
+(function(){
+  'use strict';
+
+  app.Contact = Backbone.Model.extend({
+    defaults: {
+      contactPhoto: 'img/default.png',
+      name: 'John Doe',
+      address: 'Sample Address, 55245, CA',
+      phoneNumber: '1 234 5678 9100',
+      email: 'jonh.doe@unknown.com'
+    },
+    initialize: function(){
+      $.ajaxPrefilter(function(options) {
+        options.crossDomain ={
+          crossDomain: true
+        };
+        options.xhrFields = {
+          withCredentials: true
+        };
+      });
+    }
+  });
+
+})();
